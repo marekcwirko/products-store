9b24b8c4e30407d20aba4d60d00b22c451308d62
m-cvvirko <marek@cvvirko.pl>
2020-11-18 10:41:43 +0100

    added OpenAPI

--- docs/README-myme.md
+++ docs/README-myme.md
@@ -2,4 +2,6 @@
 mvn clean flyway:migrate -Dflyway.configFile=myFlywayConfig.properties
 mvn flyway:info -Dflyway.configFile=myFlywayConfig.properties
 
-http://localhost:8080/h2-console,
+http://localhost:8080/h2-console
+https://springdoc.org/
+localhost:8080/swagger-ui.html 
--- src/main/java/pl/pgf/task/productsstore/ProductsStoreApplication.java
+++ src/main/java/pl/pgf/task/productsstore/ProductsStoreApplication.java
@@ -1,13 +1,42 @@
 package pl.pgf.task.productsstore;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductsStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductsStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsStoreApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Products Store API").version(appVersion));
+    }
+
+    @Bean
+    public GroupedOpenApi productionApi() {
+        return GroupedOpenApi.builder()
+                .group("Products Store Production")
+                .pathsToMatch("/api/v1/production/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi salesApi() {
+        return GroupedOpenApi.builder()
+                .group("Products Store Sales")
+                .pathsToMatch("/api/v1/sales/**")
+                .build();
+    }
 
 }
